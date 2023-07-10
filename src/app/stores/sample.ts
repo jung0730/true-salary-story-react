ee2d9c336ff9df598f625b79ac3bcfee3ebf86ad
Rebecca <rebecca.yu@senao.com.tw>
2023-07-10 17:25:09 +0800

    chore: dependency & register

---
+++ src/app/stores/sample.ts
@@ -0,0 +1,16 @@
+import { create } from 'zustand';
+
+// const avatarStore = (set: any) => ({
+// 	avatar: "",
+// 	setAvatar: (avatar: string) => set({ avatar }),
+// });
+
+// const useAvatarStore = create<AvatarState>()(
+// 	devtools(
+// 		persist(avatarStore, {
+// 			name: "avatar",
+// 		})
+// 	)
+// )
+
+// export default useAvatarStore;

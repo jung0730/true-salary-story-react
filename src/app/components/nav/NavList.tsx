83d88014e2339f3a58519c23f8bfcfc628d8cb05
Rebecca <rebecca.yu@senao.com.tw>
2023-08-02 09:09:18 +0800

    feat: NavList add router

--- src/app/components/nav/NavList.tsx
+++ src/app/components/nav/NavList.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useRouter } from 'next/navigation';
 import Button from '../Button'
 import Search from './Search'
 import Message from './Message'
@@ -5,21 +8,22 @@ import Point from './Point'
 import User from './User'
 
 const NavList = () => {
+  const router = useRouter();
   return (
     <>
       <div className="relative">
-        <Button color="blue-text">
+        <Button color="blue-text" onClick={() => router.push('/share')}>
           匿名分享
         </Button>
       </div>
       <Search />
       <Message />
       <Point />
-      <Button color="secondary">
+      <Button color="secondary" onClick={() => router.push('/login')}>
         登入
       </Button>
       <User />
-      <Button>
+      <Button onClick={() => router.push('/order')}>
         加薪計畫
       </Button>
     </>

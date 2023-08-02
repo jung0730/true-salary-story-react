15f71c3ce6d7642f798751e78f3d23fe82a78fb1
Rebecca <rebecca.yu@senao.com.tw>
2023-08-02 14:29:47 +0800

    feat: basic login

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,3 +1,17 @@
+import LoginAction from '../../components/Login/LoginAction'
+
 export default function Page() {
-  return <h1>Hello, Login Page!</h1>
+  return (
+    <div className="login h-screen w-screen flex justify-center items-center">
+      <div className="login-box bg-white border-2 p-10 w-10/12 md:w-[400px]">
+        <h2 className="text-blue mt-5 md:text-3xl text-2xl">有比較才有進步</h2>
+        <div className="mt-10">
+          <LoginAction />
+        </div>
+        <div className="mt-10 text-sm text-black-5">
+          繼續代表你同意真薪話「服務條款」與「隱私權政策」。真薪話不會將您的個人資訊用於任何商業用途,或公開顯示在畫面中。
+        </div>
+      </div>
+    </div>
+  )
 }
---
+++ src/app/components/Login/LoginAction.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { useLogin } from '../../services/mutation'
+import Image from 'next/image'
+import Button from '../Button';
+
+const LoginAction = () => {
+  return (
+    <Button className="w-full flex justify-center items-center" color="gray"
+            onClick={() => useLogin()}>
+      <Image
+        src="/google.svg"
+        alt=""
+        className="mr-2"
+        width={20}
+        height={20}
+      />
+      使用 Google 繼續
+    </Button>
+  )
+}
+
+export default LoginAction

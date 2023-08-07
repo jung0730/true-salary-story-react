736b1b3fa2980d315d89038eb9d0d4fdc3dd92e6
Rebecca <rebecca.yu@senao.com.tw>
2023-08-07 17:38:16 +0800

    fix form error type

--- src/app/components/FormControl/Form.tsx
+++ src/app/components/FormControl/Form.tsx
@@ -15,7 +15,7 @@ const Form = () => {
   }
   return (
     <form className="px-3 py-6 md:p-6 bg-white" onSubmit={handleSubmit(onSubmit)}>
-      <FormInput label="公司統一編號" placeholder="請輸入公司統一編號" error={`${errors.taxId?.message}`} {...register('taxId', { required: "This is required." })}/>
+      <FormInput label="公司統一編號" placeholder="請輸入公司統一編號" error={errors?.taxId?.message} {...register('taxId', { required: "This is required." })}/>
       <button type="submit" className="w-full">
         儲存
       </button>

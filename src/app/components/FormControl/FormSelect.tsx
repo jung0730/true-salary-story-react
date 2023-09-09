f9998372b70a651f3db43cc65b5a118d018e7df5
jung0730 <arj2763@gmail.com>
2023-09-09 10:40:59 +0800

    修正FormSelect元件

--- src/app/components/FormControl/FormSelect.tsx
+++ src/app/components/FormControl/FormSelect.tsx
@@ -1,19 +1,19 @@
 import clsx from 'clsx';
-import { forwardRef, ForwardedRef } from 'react';
+import { forwardRef, ForwardedRef, useState } from 'react';
 
 type FormSelectProps = {
   options: [];
-  value?: string;
-  title: string;
-  error?: string;
+  title: string,
+  onChange: (value) => void;
+  error?: string
+  name: string;
 };
 
 // label, required, error...
 const FormSelect = forwardRef((props: FormSelectProps, ref: ForwardedRef<HTMLSelectElement>) => {
   const {
-    title, options, error, ...rest
+    title, options, error, onChange, name, ...rest
   } = props;
-
   const optionsList = options.map((item) =>
     <option key={item.text}
             {...rest}
@@ -28,7 +28,7 @@ const FormSelect = forwardRef((props: FormSelectProps, ref: ForwardedRef<HTMLSel
     <div className="mb-10">
       <div className="text-black-10 mb-2">{title}</div>
       <div className="md:flex items-center justify-between">
-        <select ref={ref} className="w-full border border-black-1 rounded py-2 pl-4 pr-9">
+        <select ref={ref} name={name} onChange={onChange} className="w-full border border-black-1 rounded py-2 pl-4 pr-9">
           <option value="">請選擇</option>
           {optionsList}
         </select>

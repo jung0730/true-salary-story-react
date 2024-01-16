f8485232057619a43133cdeae8ff64bbc4eb0ccf
jung0730 <arj2763@gmail.com>
2024-01-16 18:27:49 +0800

    eslint rule: add semi

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
 // TODO
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+  return NextResponse.redirect(new URL('/home', request.url));
 }
  
 // See "Matching Paths" below to learn more

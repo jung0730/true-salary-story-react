613f6479b49de74cae7ed48cec50c9cfb8925398
jung0730 <arj2763@gmail.com>
2024-01-26 12:07:41 +0800

    安裝toast & husky

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import Provider from './provider';
 import { Inter } from 'next/font/google';
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,7 +13,8 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
+        <Toaster position="bottom-center" />
         <Provider>{children}</Provider>
       </body>
     </html>

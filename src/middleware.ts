d2937dd9243c5654aa548798388d1ced6aabd2bd
jung0730 <arj2763@gmail.com>
2024-02-02 09:04:24 +0800

    將登入檢查移動至middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,16 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
 export const config = {
   matcher: '/share/:path*',
-}
+};
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
   if (!token) {
     request.cookies.delete('token');
-    const url = request.nextUrl.clone()
-    url.pathname = '/login'
+    const url = request.nextUrl.clone();
+    url.pathname = '/login';
     return NextResponse.redirect(url);
   }
   return NextResponse.next();

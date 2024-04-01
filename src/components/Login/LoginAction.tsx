21a511adc0d412871681b7988e430a3354de6420
jung0730 <arj2763@gmail.com>
2024-04-01 18:27:52 +0800

    加入環境變數導頁

--- src/components/Login/LoginAction.tsx
+++ src/components/Login/LoginAction.tsx
@@ -6,7 +6,7 @@ const LoginAction = () => {
     <Button
       className="flex w-full items-center justify-center"
       color="gray"
-      onClick={() => (window.location.href = 'http://localhost:3000/social/google')}
+      onClick={() => (window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/social/google`)}
     >
       <Image src="/google.svg" alt="" className="mr-2" width={20} height={20} />
       使用 Google 繼續

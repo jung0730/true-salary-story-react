18cc7dc399dbdc65f71835a01232cc36c280c230
jung0730 <arj2763@gmail.com>
2024-04-08 21:43:58 +0800

    useMutation 加入isLoading

--- src/components/Checkout/Sum.tsx
+++ src/components/Checkout/Sum.tsx
@@ -3,10 +3,11 @@ import { useSearchParams } from 'next/navigation';
 import Button from '../Button';
 import { usePostOrder, usePostTransaction } from '@/services/mutation';
 import useAuthStore from '@/stores/auth';
+import LoadingAnimation from '../LoadingAnimation';
 
 const Sum = () => {
   const { user } = useAuthStore();
-  const { mutate } = usePostOrder();
+  const { mutate, isLoading } = usePostOrder();
   const { mutate: mutateTransaction } = usePostTransaction();
   const searchParams = useSearchParams();
   const type = searchParams.get('type') || '';
@@ -63,33 +64,36 @@ const Sum = () => {
     });
   };
   return (
-    <div className="my-1.5 w-full flex-col border-black-1">
-      <div className="flex w-full flex-col lg:flex-row">
-        <div className="w-full flex-col lg:border-r lg:border-black-1 lg:pe-10">
-          <div className="mb-5 flex-col border-b border-black-1 pb-5">
-            <div className="mb-2 flex justify-between">
-              <h6 className="text-black-6">現有積分</h6>
-              <h6 className="text-black-6">{user.points.point}積分</h6>
+    <>
+      {isLoading && <LoadingAnimation />}
+      <div className="my-1.5 w-full flex-col border-black-1">
+        <div className="flex w-full flex-col lg:flex-row">
+          <div className="w-full flex-col lg:border-r lg:border-black-1 lg:pe-10">
+            <div className="mb-5 flex-col border-b border-black-1 pb-5">
+              <div className="mb-2 flex justify-between">
+                <h6 className="text-black-6">現有積分</h6>
+                <h6 className="text-black-6">{user.points.point}積分</h6>
+              </div>
+              <div className="mb-2 flex justify-between">
+                <h6 className="text-black-6">本次購買積分</h6>
+                <h6 className="text-black-6">+ {result.totalPoint} 積分</h6>
+              </div>
+              <div className="flex justify-between mb-2">
+                <h5>購買後總積分</h5>
+                <h5 className="text-black-10">{getExpectedPoint()} 積分</h5>
+              </div>
             </div>
-            <div className="mb-2 flex justify-between">
-              <h6 className="text-black-6">本次購買積分</h6>
-              <h6 className="text-black-6">+ {result.totalPoint} 積分</h6>
-            </div>
-            <div className="flex justify-between mb-2">
-              <h5>購買後總積分</h5>
-              <h5 className="text-black-10">{getExpectedPoint()} 積分</h5>
+            <div className="flex justify-between">
+              <h5>小計</h5>
+              <h5>$ {result.price}元</h5>
             </div>
           </div>
-          <div className="flex justify-between">
-            <h5>小計</h5>
-            <h5>$ {result.price}元</h5>
+          <div className="flex w-full items-center justify-center lg:w-2/5">
+            <Button onClick={clickToPay}>開始付款</Button>
           </div>
-        </div>
-        <div className="flex w-full items-center justify-center lg:w-2/5">
-          <Button onClick={clickToPay}>開始付款</Button>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
